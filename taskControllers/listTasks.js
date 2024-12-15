36ee509455e5aaf4aa1954aef6af8f19af90228b
Olaitan Kehinde <ken_adex@yahoo.com>
2024-12-15 21:20:08 +0100

    adjust the logging of the list

--- taskControllers/listTasks.js
+++ taskControllers/listTasks.js
@@ -4,10 +4,10 @@ module.exports = (status) => {
   if (!status) {
     fs.readFile("db.json", "utf-8", (err, data) => {
       const db = JSON.parse(data);
-      db.forEach((task) => {
-        console.log("List of tasks:");
-        console.log("-----------------------------------------------");
+      console.log("List of tasks:");
+      console.log("-----------------------------------------------");
 
+      db.forEach((task) => {
         console.log(
           `${task.id} | ${task.description} | ${task.status} | ${task.createdAt}`
         );
@@ -16,11 +16,10 @@ module.exports = (status) => {
   } else if (["done", "todo", "in-progress"].includes(status)) {
     fs.readFile("db.json", "utf-8", (err, data) => {
       const db = JSON.parse(data);
+      console.log("List of tasks:");
+      console.log("-----------------------------------------------");
       db.forEach((task) => {
         if (task.status === status) {
-          console.log("List of tasks:");
-          console.log("-----------------------------------------------");
-
           console.log(
             `${task.id} | ${task.description} | ${task.status} | ${task.createdAt}`
           );
